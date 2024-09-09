4e1eec61e8d71e130e4ec7e1652cb6135a113fd4
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-09-10 02:00:48 +0900

    add: controller/save_product.go

---
+++ src/api/controller/save_product.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
+
+type SaveProductController struct {
+	uc usecase.SaveProductUseCase
+}
+
+func NewSaveProductController(uc usecase.SaveProductUseCase) SaveProductController {
+	return SaveProductController{
+		uc: uc,
+	}
+}
+
+func (con SaveProductController) SaveProduct(ctx *gin.Context) {
+	var input usecase.SaveProductInputDTO
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+}
