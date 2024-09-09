4ddea056fc8f904f3a9398514c8ea626fb8fec39
KakinokiKanta <kanta12621@gmail.com>
2024-09-09 21:06:48 +0900

    add: gorm周りを追加

--- src/main.go
+++ src/main.go
@@ -1,10 +1,48 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
 
 func main() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		"fes-booth-db", "user", "password", "fes-booth-db", "5432")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// var count = 0
+	// for count < 10 {
+	// 	if db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{}); err != nil {
+	// 		time.Sleep(time.Second * 5)
+	// 		count++
+	// 		log.Printf("retry... count:%v\n", count)
+	// 		continue
+	// 	}
+	// 	break
+	// }
+	if err != nil {
+		panic("failed to connect database")
+	} else {
+		println("connection is successful")
+	}
+	db.AutoMigrate(&User{})
+
+	user := User{
+		Name:  "user1",
+		Email: "user1@user.com",
+	}
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		db.Create(&user)
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
