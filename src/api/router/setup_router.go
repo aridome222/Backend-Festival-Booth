5d226012538cc97f56125440f9891da72ce328a7
KakinokiKanta <kanta12621@gmail.com>
2024-09-11 00:27:33 +0900

    add: /usersのGET用エンドポイントのルーティングを追加

--- src/api/router/setup_router.go
+++ src/api/router/setup_router.go
@@ -9,14 +9,21 @@ import (
 )
 
 func SetupRouter(db *gorm.DB) {
+	// save product
 	productRepository := repository.NewProductRepository(db)
 	saveProductUseCase := usecase.NewSaveProductUseCase(productRepository)
 	saveProductController := controller.NewSaveProductController(saveProductUseCase)
+
+	// get users
+	userRepository := repository.NewUserRepository(db)
+	getUserUseCase := usecase.NewGetUserUseCase(userRepository)
+	getUserController := controller.NewGetUserController(getUserUseCase)
 
 	r := gin.Default()
 	// TODO: cors設定を追記
 
 	r.POST("/products", saveProductController.SaveProduct)
+	r.GET("/users", getUserController.GetUser)
 
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
 }
