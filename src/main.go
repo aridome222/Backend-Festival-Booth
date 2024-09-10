180730b7137f51b5b27dae6ecbc6b26192af8271
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-09-10 21:30:18 +0900

    update: 各層の構造体のインスタンスを生成し、依存性の注入を行う処理を追加

--- src/main.go
+++ src/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aridome222/Backend-Festival-Booth/api/controller"
+	"github.com/aridome222/Backend-Festival-Booth/infrastructure/repository"
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -40,12 +43,19 @@ func main() {
 		panic("failed to migrate")
 	}
 
+	productRepository := repository.NewProductRepository(db)
+	saveProductUseCase := usecase.NewSaveProductUseCase(productRepository)
+	saveProductController := controller.NewSaveProductController(saveProductUseCase)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.POST("/products", saveProductController.SaveProduct)
+
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
 }
 
