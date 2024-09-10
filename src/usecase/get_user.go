c1ad8de533b0abaa2854fcd837d3ba2adb81ad76
KakinokiKanta <kanta12621@gmail.com>
2024-09-10 16:57:03 +0900

    add: GetUserのDTOを定義

--- src/usecase/get_user.go
+++ src/usecase/get_user.go
@@ -6,6 +6,20 @@ type GetUserUseCase struct {
 	repo domain.UserRepository
 }
 
+type GetUserUseCaseInputDTO struct {
+	Page  int
+	Limit int
+}
+
+type GetUserUseCaseOutputDTO struct {
+	ID           string
+	Name         string
+	Introduction string
+	IconNum      int
+	GithubUrl    string
+	XUrl         string
+}
+
 func NewGetUserUseCase(repo domain.UserRepository) GetUserUseCase {
 	return GetUserUseCase{
 		repo: repo,
