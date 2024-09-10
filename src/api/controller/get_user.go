a9ecb3ddf15c065a75047cb134e82290d8a3d506
KakinokiKanta <kanta12621@gmail.com>
2024-09-10 17:46:47 +0900

    add: GetUserのコントローラーを実装

--- src/api/controller/get_user.go
+++ src/api/controller/get_user.go
@@ -1,6 +1,12 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type GetUserController struct {
 	uc usecase.GetUserUseCase
@@ -10,4 +16,20 @@ func NewGetUserController(uc usecase.GetUserUseCase) GetUserController {
 	return GetUserController{
 		uc: uc,
 	}
+}
+
+func (con GetUserController) GetUser(ctx *gin.Context) {
+	var input usecase.GetUserUseCaseInputDTO
+
+	input.Page, _ = strconv.Atoi(ctx.DefaultQuery("page", "-1"))
+	input.Limit, _ = strconv.Atoi(ctx.DefaultQuery("limit", "-1"))
+
+	output, err := con.uc.GetUser(input)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, output)
 }
