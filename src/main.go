198b1dfd13c52d2c1ec7f81d05bd66cf1a65bef6
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-09-10 23:10:00 +0900

    refactor: ルーターの切り出し

--- src/main.go
+++ src/main.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"github.com/aridome222/Backend-Festival-Booth/api/controller"
+	"github.com/aridome222/Backend-Festival-Booth/api/router"
 	"github.com/aridome222/Backend-Festival-Booth/infrastructure"
-	"github.com/aridome222/Backend-Festival-Booth/infrastructure/repository"
-	"github.com/aridome222/Backend-Festival-Booth/usecase"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db := infrastructure.ConnectDB()
 
-	productRepository := repository.NewProductRepository(db)
-	saveProductUseCase := usecase.NewSaveProductUseCase(productRepository)
-	saveProductController := controller.NewSaveProductController(saveProductUseCase)
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.POST("/products", saveProductController.SaveProduct)
-
-	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
+	router.SetupRouter(db)
 }
