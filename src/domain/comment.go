c88ec9df7b26ac2dd14ba77f230a4954696f2c81
KakinokiKanta <kanta12621@gmail.com>
2024-09-12 02:06:39 +0900

    add: Commentドメインとファクトリー関数を追加

--- src/domain/comment.go
+++ src/domain/comment.go
@@ -1,1 +1,22 @@
 package domain
+
+type Comment struct {
+	ID        string `gorm:"primary_key"`
+	ProductID string `gorm:"not null"`
+	UserName  string `gorm:"not null"`
+	Message   string `gorm:"not null"`
+}
+
+func NewComment(
+	id string,
+	productID string,
+	userName string,
+	message string,
+) Comment {
+	return Comment{
+		ID:        id,
+		ProductID: productID,
+		UserName:  userName,
+		Message:   message,
+	}
+}
