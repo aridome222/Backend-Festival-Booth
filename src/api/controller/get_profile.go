1947ab9a2664cd196a1c3755e82f0b1cffff5ca9
KakinokiKanta <kanta12621@gmail.com>
2024-09-11 22:48:06 +0900

    add: ユーザ名からユーザ情報を取得するコントローラー実装

--- src/api/controller/get_profile.go
+++ src/api/controller/get_profile.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"net/http"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type GetProfileController struct {
 	uc usecase.GetProfileUseCase
@@ -10,4 +15,20 @@ func NewGetProfileController(uc usecase.GetProfileUseCase) GetProfileController 
 	return GetProfileController{
 		uc: uc,
 	}
+}
+
+func (con GetProfileController) GetProfile(ctx *gin.Context) {
+	var input usecase.GetProfileUseCaseInputDTO
+	var err error
+
+	input.UserName = ctx.Param("name")
+
+	output, err := con.uc.GetProfile(input)
+
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, output)
 }
