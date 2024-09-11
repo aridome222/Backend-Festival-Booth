a1059bc6d98e903aa007c49d248cd1397d1e57bd
KakinokiKanta <kanta12621@gmail.com>
2024-09-11 16:56:42 +0900

    add: GetProductユースケースのDTOを定義

--- src/usecase/get_product.go
+++ src/usecase/get_product.go
@@ -6,6 +6,18 @@ type GetProductUseCase struct {
 	repo repository.ProductRepository
 }
 
+type GetProductUseCaseInputDTO struct {
+	Page  int
+	Limit int
+}
+
+type GetProductUseCaseOutputDTO struct {
+	ProductID   string `json:"product_id"`
+	UserName    string `json:"user_name"`
+	Url         string `json:"url"`
+	Description string `json:"description"`
+}
+
 func NewGetProductUseCase(repo repository.ProductRepository) GetProductUseCase {
 	return GetProductUseCase{
 		repo: repo,
