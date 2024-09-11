a5eefd174890276d63e7faa83ac9e6aefb07cc2e
KakinokiKanta <kanta12621@gmail.com>
2024-09-11 21:37:23 +0900

    add: 成果物を取得するユースケースを実装するために、DTOを定義

--- src/usecase/get_product.go
+++ src/usecase/get_product.go
@@ -6,6 +6,17 @@ type GetProductUseCase struct {
 	repo domain.Product
 }
 
+type GetProductUseCaseInputDTO struct {
+	UserName string
+}
+
+type GetProductUseCaseOutputDTO struct {
+	ProductID   string `json:"product_id"`
+	UserName    string `json:"user_name"`
+	Url         string `json:"url"`
+	Description string `json:"description"`
+}
+
 func NewGetProductUseCase(repo domain.Product) GetProductUseCase {
 	return GetProductUseCase{
 		repo: repo,
