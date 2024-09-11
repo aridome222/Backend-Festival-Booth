0dc5853c205c7b17bd5efda9d149dd86f2593d67
KakinokiKanta <kanta12621@gmail.com>
2024-09-11 17:03:07 +0900

    add: GetProductのユースケースを実装

--- src/usecase/get_product.go
+++ src/usecase/get_product.go
@@ -1,6 +1,9 @@
 package usecase
 
-import "github.com/aridome222/Backend-Festival-Booth/infrastructure/repository"
+import (
+	"github.com/aridome222/Backend-Festival-Booth/domain"
+	"github.com/aridome222/Backend-Festival-Booth/infrastructure/repository"
+)
 
 type GetProductUseCase struct {
 	repo repository.ProductRepository
@@ -22,4 +25,32 @@ func NewGetProductUseCase(repo repository.ProductRepository) GetProductUseCase {
 	return GetProductUseCase{
 		repo: repo,
 	}
+}
+
+func (uc GetProductUseCase) GetProduct(input GetProductUseCaseInputDTO) ([]GetProductUseCaseOutputDTO, error) {
+	var products []domain.Product
+	var err error
+
+	if input.Page < 0 || input.Limit < 0 {
+		products, err = uc.repo.FindAll()
+	} else {
+		products, err = uc.repo.Find(input.Page, input.Limit)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	outputSlice := []GetProductUseCaseOutputDTO{}
+
+	for _, product := range products {
+		outputSlice = append(outputSlice, GetProductUseCaseOutputDTO{
+			product.ProductID,
+			product.UserName,
+			product.Url,
+			product.Description,
+		})
+	}
+
+	return outputSlice, nil
 }
