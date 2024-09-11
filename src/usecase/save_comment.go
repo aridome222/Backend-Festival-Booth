204843fb545fa530da506308af921389e82f9f94
KakinokiKanta <kanta12621@gmail.com>
2024-09-12 02:26:46 +0900

    add: コメントを保存するユースケースを実装するために、DTOを定義

--- src/usecase/save_comment.go
+++ src/usecase/save_comment.go
@@ -6,6 +6,17 @@ type SaveCommentUseCase struct {
 	repo domain.CommentRepository
 }
 
+type SaveCommentUseCaseInputDTO struct {
+	ProductID string `json:"product_id" binding:"required"`
+	Message   string `json:"message" binding:"required,min=1,max=100"`
+}
+
+type SaveCommentUseCaseOutputDTO struct {
+	ID        string `json:"id"`
+	ProductID string `json:"product_id"`
+	Message   string `json:"message"`
+}
+
 func NewCommentUseCase(repo domain.CommentRepository) SaveCommentUseCase {
 	return SaveCommentUseCase{
 		repo: repo,
