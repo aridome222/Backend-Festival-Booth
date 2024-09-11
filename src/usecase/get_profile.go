02db55b0dda2b5a9f232da91dcc13f05c15ce9c7
KakinokiKanta <kanta12621@gmail.com>
2024-09-11 22:40:59 +0900

    add: ユーザ名からユーザ情報を取得するユースケース実装のために、DTOを定義

--- src/usecase/get_profile.go
+++ src/usecase/get_profile.go
@@ -6,6 +6,19 @@ type GetProfileUseCase struct {
 	repo domain.ProfileRepository
 }
 
+type GetProfileUseCaseInputDTO struct {
+	UserName string
+}
+
+type GetProfileUseCaseOutputDTO struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Introduction string `json:"introduction"`
+	IconNum      int    `json:"icon_num"`
+	GithubUrl    string `json:"github_url"`
+	XUrl         string `json:"x_url"`
+}
+
 func NewGetProfileUseCase(repo domain.ProfileRepository) GetProfileUseCase {
 	return GetProfileUseCase{
 		repo: repo,
