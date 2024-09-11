b1f330899fcda3f786d63a209fd12217f5079f48
KakinokiKanta <kanta12621@gmail.com>
2024-09-12 02:48:34 +0900

    add: コメントを保存するコントローラーを実装

--- src/api/controller/save_comment.go
+++ src/api/controller/save_comment.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"net/http"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type SaveCommentController struct {
 	uc usecase.SaveCommentUseCase
@@ -10,4 +15,21 @@ func NewCommentController(uc usecase.SaveCommentUseCase) SaveCommentController {
 	return SaveCommentController{
 		uc: uc,
 	}
+}
+
+func (con SaveCommentController) SaveComment(ctx *gin.Context) {
+	var input usecase.SaveCommentUseCaseInputDTO
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	savedComment, err := con.uc.SaveComment(input)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, savedComment)
 }
