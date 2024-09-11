40335ce7ad0e427986b8abd8ad8459133f7a394b
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-09-12 01:36:24 +0900

    update: SaveProfileControllerのコンストラクタを実装

--- src/api/controller/save_profile.go
+++ src/api/controller/save_profile.go
@@ -5,3 +5,9 @@ import "github.com/aridome222/Backend-Festival-Booth/usecase"
 type SaveProfileController struct {
 	uc usecase.SaveProfileUsecase
 }
+
+func NewSaveProfileController(uc usecase.SaveProfileUsecase) SaveProfileController {
+	return SaveProfileController{
+		uc: uc,
+	}
+}
