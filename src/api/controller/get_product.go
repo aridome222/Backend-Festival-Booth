969d3d7596767c204d0c5bbe71f9255177c20f72
KakinokiKanta <kanta12621@gmail.com>
2024-09-11 21:55:34 +0900

    add: 成果物を取得するコントローラーを追加

--- src/api/controller/get_product.go
+++ src/api/controller/get_product.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"net/http"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type GetProductController struct {
 	uc usecase.GetProductUseCase
@@ -10,4 +15,20 @@ func NewGetProductController(uc usecase.GetProductUseCase) GetProductController 
 	return GetProductController{
 		uc: uc,
 	}
+}
+
+func (con GetProductController) GetProduct(ctx *gin.Context) {
+	var input usecase.GetProductUseCaseInputDTO
+	var err error
+
+	input.UserName = ctx.Param("user_name")
+
+	output, err := con.uc.GetProduct(input)
+
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, output)
 }
