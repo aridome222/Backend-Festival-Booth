9c67c6f1f576a92c660f2f3035adba2b2bef92fc
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-09-12 05:04:34 +0900

    update: 構造体SaveProfileContorollerのメソッドSaveProfileを実装

--- src/api/controller/save_profile.go
+++ src/api/controller/save_profile.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"net/http"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type SaveProfileController struct {
 	uc usecase.SaveProfileUsecase
@@ -10,4 +15,24 @@ func NewSaveProfileController(uc usecase.SaveProfileUsecase) SaveProfileControll
 	return SaveProfileController{
 		uc: uc,
 	}
+}
+
+func (con SaveProfileController) SaveProfile(ctx *gin.Context) {
+	// JSONデータをInputDTOの形式に合わせて格納する変数
+	var input usecase.SaveProfileInputDTO
+
+	// リクエストから取り出したJSONデータを構造体にバインド
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 自己紹介を保存するメソッドを実行
+	savedProfile, err := con.uc.SaveProfile(input)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, savedProfile)
 }
