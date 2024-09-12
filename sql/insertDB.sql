e7928489592e5a0d890486e802c181828484bce5
KakinokiKanta <kanta12621@gmail.com>
2024-09-12 19:05:36 +0900

    fix: テーブル名を修正

--- sql/insertDB.sql
+++ sql/insertDB.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (id, name, introduction, icon_num, github_url, x_url) VALUES ('u1', 'u1_name', 'u1_intro', 0, 'u1_github_url', 'u1_x_url');
-INSERT INTO users (id, name, introduction, icon_num, github_url, x_url) VALUES ('u2', 'u2_name', 'u2_intro', 0, 'u2_github_url', 'u2_x_url');
-INSERT INTO users (id, name, introduction, icon_num, github_url, x_url) VALUES ('u3', 'u3_name', 'u3_intro', 0, 'u3_github_url', 'u3_x_url');
-INSERT INTO users (id, name, introduction, icon_num, github_url, x_url) VALUES ('u4', 'u4_name', 'u4_intro', 0, 'u4_github_url', 'u4_x_url');
-INSERT INTO users (id, name, introduction, icon_num, github_url, x_url) VALUES ('u5', 'u5_name', 'u5_intro', 0, 'u5_github_url', 'u5_x_url');
+INSERT INTO profiles (id, name, introduction, icon_num, github_url, x_url) VALUES ('u1', 'u1_name', 'u1_intro', 0, 'u1_github_url', 'u1_x_url');
+INSERT INTO profiles (id, name, introduction, icon_num, github_url, x_url) VALUES ('u2', 'u2_name', 'u2_intro', 0, 'u2_github_url', 'u2_x_url');
+INSERT INTO profiles (id, name, introduction, icon_num, github_url, x_url) VALUES ('u3', 'u3_name', 'u3_intro', 0, 'u3_github_url', 'u3_x_url');
+INSERT INTO profiles (id, name, introduction, icon_num, github_url, x_url) VALUES ('u4', 'u4_name', 'u4_intro', 0, 'u4_github_url', 'u4_x_url');
+INSERT INTO profiles (id, name, introduction, icon_num, github_url, x_url) VALUES ('u5', 'u5_name', 'u5_intro', 0, 'u5_github_url', 'u5_x_url');
 
 INSERT INTO products (id, title, user_name, url, description) VALUES ('p1', 'p1_title', 'u1_name', 'p1_url', 'p1_description');
 INSERT INTO products (id, title, user_name, url, description) VALUES ('p2', 'p2_title', 'u1_name', 'p2_url', 'p2_description');
