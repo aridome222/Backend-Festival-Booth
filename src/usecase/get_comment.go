22022f8ea8a4483995ea0220de3faac2cfb018e0
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-09-12 22:03:21 +0900

    update:  成果物からコメントを取得するユースケース実装のために、DTOを定義

--- src/usecase/get_comment.go
+++ src/usecase/get_comment.go
@@ -6,6 +6,16 @@ type GetCommentUseCase struct {
 	repo domain.CommentRepository
 }
 
+type GetCommentInputDTO struct {
+	Message string
+}
+
+type GetCommentOutputDTO struct {
+	ID        string
+	ProductID string
+	Message   string
+}
+
 func NewGetCommentUseCase(repo domain.CommentRepository) GetCommentUseCase {
 	return GetCommentUseCase{
 		repo: repo,
