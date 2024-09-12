903b15879d799e7cb8157fa9d5728088dc252090
KakinokiKanta <kanta12621@gmail.com>
2024-09-12 21:50:38 +0900

    add: ユーザーアカウント作成のためのユースケース実装

---
+++ src/usecase/create_account.go
@@ -0,0 +1,52 @@
+package usecase
+
+import (
+	"github.com/aridome222/Backend-Festival-Booth/domain"
+	"github.com/oklog/ulid/v2"
+)
+
+type CreateAccountUseCase struct {
+	repo domain.AccountRepository
+}
+
+type CreateAccountInputDTO struct {
+	UserName string `json:"user_name" binding:"required,min=1,max=20"`
+	Password string `json:"password" binding:"required,min=1"`
+}
+
+type CreateAccountOutputDTO struct {
+	ID       string `json:"id"`
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
+
+func NewCreateAccountUseCase(repo domain.AccountRepository) CreateAccountUseCase {
+	return CreateAccountUseCase{
+		repo: repo,
+	}
+}
+
+func (uc CreateAccountUseCase) CreateAccount(input CreateAccountInputDTO) (CreateAccountOutputDTO, error) {
+	// 同一アカウントが存在しないか確認
+	account, err := uc.repo.FindByName(input.UserName)
+
+	// TODO: アカウントが存在しなかった場合以外のエラー処理を記述
+	if err == nil {
+		return CreateAccountOutputDTO{}, nil
+	}
+
+	account = domain.NewAccount(ulid.Make().String(), input.UserName, input.Password)
+
+	// 同一アカウントが存在しない場合はDBに保存
+	account, err = uc.repo.Create(account)
+
+	if err != nil {
+		return CreateAccountOutputDTO{}, err
+	}
+
+	return CreateAccountOutputDTO{
+		account.ID,
+		account.Name,
+		account.Password,
+	}, nil
+}
