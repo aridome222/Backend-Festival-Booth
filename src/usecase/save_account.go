e02c722474746bcc67bab09684c910a07e3ca73f
KakinokiKanta <kanta12621@gmail.com>
2024-09-12 21:11:47 +0900

    add: ユーザーアカウントを作成するユースケース実装のために、DTOを実装

--- src/usecase/save_account.go
+++ src/usecase/save_account.go
@@ -6,6 +6,17 @@ type SaveAccountUseCase struct {
 	repo domain.AccountRepository
 }
 
+type SaveAccountInputDTO struct {
+	UserName string `json:"user_name" binding:"required,min=1,max=20"`
+	Password string `json:"password" binding:"required,min=1"`
+}
+
+type SaveAccountOutputDTO struct {
+	ID       string `json:"id"`
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
+
 func NewSaveAccountUseCase(repo domain.AccountRepository) SaveAccountUseCase {
 	return SaveAccountUseCase{
 		repo: repo,
