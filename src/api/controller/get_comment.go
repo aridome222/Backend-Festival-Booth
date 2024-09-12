660feb80dffafa7433022b5347db1518bfbe2adb
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-09-13 01:32:33 +0900

    update: コメント取得リクエストを処理するGetCommentメソッドを実装

--- src/api/controller/get_comment.go
+++ src/api/controller/get_comment.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"net/http"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type GetCommentController struct {
 	uc usecase.GetCommentUseCase
@@ -10,4 +15,19 @@ func NewGetCommentController(uc usecase.GetCommentUseCase) GetCommentController 
 	return GetCommentController{
 		uc: uc,
 	}
+}
+
+func (con GetCommentController) GetComment(ctx *gin.Context) {
+	var input usecase.GetCommentInputDTO
+
+	input.ProductID = ctx.Param("product_id")
+
+	output, err := con.uc.GetComment(input)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, output)
 }
