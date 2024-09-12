4dfad2598fa2e39f8bd0264967347f5633d3395a
KakinokiKanta <kanta12621@gmail.com>
2024-09-13 02:54:15 +0900

    add: ログイン処理におけるコントローラーを実装

--- src/api/controller/login.go
+++ src/api/controller/login.go
@@ -1,6 +1,15 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"errors"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type LoginController struct {
 	uc usecase.LoginUseCase
@@ -10,4 +19,44 @@ func NewLoginController(uc usecase.LoginUseCase) LoginController {
 	return LoginController{
 		uc: uc,
 	}
+}
+
+func (con LoginController) Login(ctx *gin.Context) {
+	var input usecase.LoginInputDTO
+
+	// jsonデータを構造体にデコードしバインド
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// ログイン処理に成功したか判定
+	accountName, isLogin := con.uc.Login(input)
+	if !isLogin {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": errors.New("failed to login")})
+		return
+	}
+
+	// JWTの発行
+	claims := jwt.MapClaims{
+		"user_name": accountName,
+		"exp":       time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	secretKey := os.Getenv("SECRET_KEY")
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": errors.New("failed to generate token")},
+		)
+		return
+	}
+
+	// cookieにjwtをセット
+	ctx.SetCookie("token", tokenString, 3600, "/", "localhost", false, true)
+	ctx.JSON(http.StatusOK, gin.H{
+		"user_name": accountName,
+	})
 }
