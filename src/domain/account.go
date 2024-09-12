8361aad690d9b7e98523f8d949370326543605eb
KakinokiKanta <kanta12621@gmail.com>
2024-09-12 22:23:41 +0900

    add: ハッシュ化されたパスワードと元のパスワードで一致しているか検証する関数をドメインに追加

--- src/domain/account.go
+++ src/domain/account.go
@@ -1,5 +1,7 @@
 package domain
 
+import "golang.org/x/crypto/bcrypt"
+
 type Account struct {
 	ID       string `gorm:"primary_key"`
 	Name     string `gorm:"unique;not null"`
@@ -15,10 +17,21 @@ func NewAccount(
 	id string,
 	name string,
 	password string,
-) Account {
+) (Account, error) {
+	// パスワードをハッシュ化
+	hash_pass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return Account{}, err
+	}
+
 	return Account{
 		ID:       id,
 		Name:     name,
-		Password: password,
-	}
+		Password: string(hash_pass),
+	}, nil
+}
+
+func (account Account) IsValidPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(password))
+	return err != bcrypt.ErrMismatchedHashAndPassword
 }
