7ddd009c7929bf69114a83501bd062d5f4a44980
KakinokiKanta <kanta12621@gmail.com>
2024-09-13 17:09:44 +0900

    update: コメントドメインにユーザ名のフィールドを追加

--- src/domain/comment.go
+++ src/domain/comment.go
@@ -2,6 +2,7 @@ package domain
 
 type Comment struct {
 	ID        string `gorm:"primary_key"`
+	UserName  string `gorm:"not null"`
 	ProductID string `gorm:"not null"`
 	Message   string `gorm:"not null"`
 }
