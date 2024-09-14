91a69b8b682f7a956ce8adada030424e8c4b0c6c
KakinokiKanta <kanta12621@gmail.com>
2024-09-14 14:09:08 +0900

    add: アンケート結果を保存するハンドラーを実装

--- src/api/controller/save_group.go
+++ src/api/controller/save_group.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/aridome222/Backend-Festival-Booth/usecase"
+import (
+	"net/http"
+
+	"github.com/aridome222/Backend-Festival-Booth/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type SaveGroupController struct {
 	uc usecase.SaveGroupUseCase
@@ -10,4 +15,29 @@ func NewGroupController(uc usecase.SaveGroupUseCase) SaveGroupController {
 	return SaveGroupController{
 		uc: uc,
 	}
+}
+
+func (con SaveGroupController) SaveGroup(ctx *gin.Context) {
+	var input usecase.SaveGroupInputDTO
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// cookieからユーザー名を取得
+	user_name, exists := ctx.Get("user")
+	if !exists {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+	input.UserName = user_name.(string)
+
+	savedAccount, err := con.uc.SaveGroup(input)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, savedAccount)
 }
