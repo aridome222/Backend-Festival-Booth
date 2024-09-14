38941c9d88721c6910ca21ced387faf588077971
KakinokiKanta <kanta12621@gmail.com>
2024-09-14 13:13:00 +0900

    add: アンケート結果を保持するユースケースのために、ファクトリー関数を実装

--- src/usecase/save_group.go
+++ src/usecase/save_group.go
@@ -2,7 +2,7 @@ package usecase
 
 import "github.com/aridome222/Backend-Festival-Booth/domain"
 
-type SaveGroup struct {
+type SaveGroupUseCase struct {
 	repo domain.AccountRepository
 }
 
@@ -13,4 +13,14 @@ type SaveGroupInputDTO struct {
 type SaveGroupOutputDTO struct {
 	UserName string `json:"name"`
 	Group    string `json:"group"`
+}
+
+func NewSaveGroupUseCase(repo domain.AccountRepository) SaveGroupUseCase {
+	return SaveGroupUseCase{
+		repo: repo,
+	}
+}
+
+func (uc SaveGroupUseCase) SaveGroup(input SaveGroupInputDTO) (SaveGroupOutputDTO, error) {
+
 }
